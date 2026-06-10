c8d81c70e9a0b61f1977d4bab8ab8972dbe296e2
vielakb <vielakborja@gmail.com>
2026-06-10 09:26:01 -0500

    eliminacion de duplicacion

--- src/pages/RealizarReseña.test.js
+++ src/pages/RealizarReseña.test.js
@@ -30,43 +30,7 @@ jest.mock('../services/supabaseClient', () => ({
   },
 }))
 
-jest.mock('react', () => {
-  const actualReact = jest.requireActual('react')
-  const stateStore = []
-  let cursor = 0
-
-  const useState = jest.fn((initialValue) => {
-    const stateIndex = cursor
-    cursor += 1
-
-    if (stateStore[stateIndex] === undefined) {
-      stateStore[stateIndex] = initialValue
-    }
-
-    const setState = (value) => {
-      stateStore[stateIndex] = typeof value === 'function' ? value(stateStore[stateIndex]) : value
-    }
-
-    return [stateStore[stateIndex], setState]
-  })
-
-  const useEffect = jest.fn((callback) => {
-    callback()
-  })
-
-  return {
-    ...actualReact,
-    useState,
-    useEffect,
-    __resetHooks: () => {
-      stateStore.length = 0
-      cursor = 0
-    },
-    __resetCursor: () => {
-      cursor = 0
-    },
-  }
-})
+require('../testUtils/reactMock')
 
 const React = require('react')
 const RealizarReseña = require('./RealizarReseña.jsx').default
